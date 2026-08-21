c5bc367ce45786abde16cc1208983a8290e202a9
k1ssa1 <samikissai.edu@gmail.com>
2026-08-21 04:41:16 +0000

    feat: scrape categories from homepage aside menu

---
+++ categories/scrape_categories.py
@@ -0,0 +1,26 @@
+import requests
+from bs4 import BeautifulSoup
+from pydantic import BaseModel
+
+
+class Category(BaseModel):
+    name: str
+
+
+def scrape_categories():
+    with requests.session() as session:
+        url = "https://books.toscrape.com/index.html"
+        res = session.get(url, timeout=10)
+
+        soup = BeautifulSoup(res.text, "html.parser")
+        aside_menu = soup.find("ul", class_="nav nav-list")
+        links = aside_menu.find_all("a")
+
+        category_list = []
+
+        for l in links:
+            extracted_name = l.text.strip()
+            category = Category(name=extracted_name)
+            category_list.append(category)
+
+        print(category_list)
--- main.py
+++ main.py
@@ -1,8 +1,10 @@
+from categories.scrape_categories import scrape_categories
 from homepage.scrape_homepage import scrape_catalogue
 
 
 def main():
     scrape_catalogue()
+    scrape_categories()
 
 
 if __name__ == "__main__":
